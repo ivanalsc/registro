33e1ab5d7d0c3dbcbb0dfd2f7f098c1b3420c4be
ivanalsc <ivanasosacordero@gmail.com>
2020-09-07 13:57:47 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+gsap.from(".imagen", { duration: 6, opacity: 0, ease: "rough" });
+gsap.to(".imagen", { duration: 2, x: 100, ease: "bounce" });
+gsap.to(".logo", { duration: 6, x: 30, ease: "bounce" });
+gsap.from(".form", { duration: 6, opacity: 0, ease: "rough" });
+gsap.from("nav", { duration: 6, opacity: 0, ease: "rough" });
+gsap.from("footer", { duration: 6, opacity: 0, ease: "rough" });
